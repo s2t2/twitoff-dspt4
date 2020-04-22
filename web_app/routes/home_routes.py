d7e0faf7669cf28fcebf752c81491d7df00fd071
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-21 23:07:42 -0400

    Refactor using app factory pattern

---
+++ web_app/routes/home_routes.py
@@ -0,0 +1,16 @@
+
+# web_app/routes/home_routes.py
+
+from flask import Blueprint
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+def hello_world():
+    print("YOU VISITED THE HOMEPAGE")
+    return "Hello, World!"
+
+@home_routes.route("/about")
+def about():
+    print("YOU VISITED THE ABOUT PAGE")
+    return "About Me (TODO)"
