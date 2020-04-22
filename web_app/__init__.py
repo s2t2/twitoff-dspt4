aead634aff244a0c3e686ff222f38b091f250109
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-22 00:32:56 -0400

    Integrating with the db

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -12,6 +12,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
 
--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -2,25 +2,32 @@
 
 from flask import Blueprint, jsonify, render_template, request #, flash, redirect
 
+from web_app.models import db, Book, parse_records
+
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
-    return jsonify(books)
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #]
+    book_records = Book.query.all()
+    print(book_records)
+    books_response = parse_records(book_records)
+    return jsonify(books_response)
 
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
-    return render_template("books.html", message="Here's some books", books=books)
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #]
+    book_records = Book.query.all()
+    print(book_records)
+    return render_template("books.html", message="Here's some books", books=book_records)
 
 @book_routes.route("/books/new")
 def new_book():
@@ -28,10 +35,14 @@ def new_book():
 
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
-    print("FORM DATA:", dict(request.form))
-    # todo: store in database
+    print("FORM DATA:", dict(request.form)) #> {"book_title": "___", "author_name": "____"}
+
+    new_book = Book(title=request.form["book_title"], author_id=request.form["author_name"])
+    db.session.add(new_book)
+    db.session.commit()
+
     return jsonify({
-        "message": "BOOK CREATED OK (TODO)",
+        "message": "BOOK CREATED OK",
         "book": dict(request.form)
     })
     #flash(f"Book '{new_book.title}' created successfully!", "success")
