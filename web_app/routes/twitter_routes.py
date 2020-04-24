19ac20bb4426ae7fcb8ac7aba814c6db13766fcd
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-24 00:21:43 -0400

    Store user info in db

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint, jsonify #, render_template, request, flash, redirect
 
-#from web_app.models import db, Book, parse_records
+from web_app.models import db, User, Tweet, parse_records
 from web_app.services.twitter_service import api as twitter_api
 
 twitter_routes = Blueprint("twitter_routes", __name__)
@@ -11,15 +11,21 @@ twitter_routes = Blueprint("twitter_routes", __name__)
 def fetch_user_data(screen_name):
     print("FETCHING...", screen_name)
 
-    # TODO: fetch user info
+    # fetch user info
     user = twitter_api.get_user(screen_name)
+    # store user info in database
+    db_user = User.query.get(user.id) or User(id=user.id)
+    db_user.screen_name = user.screen_name
+    db_user.name = user.name
+    db_user.location = user.location
+    db_user.followers_count = user.followers_count
+    db.session.add(db_user)
+    db.session.commit()
 
-    # TODO: fetch their tweets
+    # fetch their tweets
     statuses = twitter_api.user_timeline(screen_name, tweet_mode="extended", count=35, exclude_replies=True, include_rts=False)
 
     # TODO: fetch embedding for each tweet
-
-    # TODO: store user info in database
     # TODO: store tweets in database (w/ embeddings)
 
     #return f"FETCHED {screen_name} OK"
