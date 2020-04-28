3945c2d0cdaa605ee3dbd0d489c07bbd176a69e1
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-27 23:05:49 -0400

    Refactor using pre-trained model

--- web_app/routes/stats_routes.py
+++ web_app/routes/stats_routes.py
@@ -3,14 +3,15 @@ from sklearn.linear_model import LogisticRegression
 
 from flask import Blueprint, jsonify, request, flash, redirect # render_template
 
+from web_app.statsmodels import load_model
+
 stats_routes = Blueprint("stats_routes", __name__)
 
 # TODO: accept some inputs related to the iris training data (x values)
 @stats_routes.route("/stats/iris")
 def iris():
     X, y = load_iris(return_X_y=True)
-    clf = LogisticRegression(random_state=0, solver="lbfgs", multi_class="multinomial")
-    clf.fit(X, y)
+    clf = load_model() # make sure to pre-train the model first!
     result = str(clf.predict(X[:2, :]))
     print("PREDICTION", result)
     return result # todo: return as JSON
