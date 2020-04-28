c1e819ad04bd168d26021b807750a06bff029643
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-28 00:23:48 -0400

    Make predictions

--- web_app/routes/stats_routes.py
+++ web_app/routes/stats_routes.py
@@ -1,9 +1,11 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 
-from flask import Blueprint, jsonify, request, flash, redirect # render_template
+from flask import Blueprint, jsonify, request, flash, redirect, render_template
 
+from web_app.models import User
 from web_app.statsmodels import load_model
+from web_app.services.basilica_service import connection as basilica_connection
 
 stats_routes = Blueprint("stats_routes", __name__)
 
@@ -16,7 +18,6 @@ def iris():
     print("PREDICTION", result)
     return result # todo: return as JSON
 
-# TODO: accept some inputs related to the iris training data (x values)
 @stats_routes.route("/stats/predict", methods=["POST"])
 def twitoff_predict():
     print("PREDICT ROUTE...")
@@ -26,8 +27,36 @@ def twitoff_predict():
     tweet_text = request.form["tweet_text"]
     print(screen_name_a, screen_name_b, tweet_text)
 
-    # TODO: train a model
+    #
+    # train the model
+    #
 
-    # TODO: make a prediction and return it
+    tweet_embeddings = []
+    tweet_labels = []
+    user_a = User.query.filter(User.screen_name == screen_name_a).one()
+    user_b = User.query.filter(User.screen_name == screen_name_b).one()
+    tweets_a = user_a.tweets
+    tweets_b = user_b.tweets
+    all_tweets = tweets_a + tweets_b
+    for tweet in all_tweets:
+        tweet_embeddings.append(tweet.embedding)
+        tweet_labels.append(tweet.user.screen_name)
+    print("EMBEDDINGS:", len(tweet_embeddings), "LABELS:", len(tweet_labels))
 
-    return "OK (TODO)"
+    classifier = LogisticRegression(random_state=0, solver="lbfgs", multi_class="multinomial")
+    classifier.fit(tweet_embeddings, tweet_labels)
+
+    #
+    # make a prediction
+    #
+
+    example_tweet_embedding = basilica_connection.embed_sentence(tweet_text, model="twitter")
+    result = classifier.predict([example_tweet_embedding])
+    print("RESULT:", result[0])
+
+    return render_template("prediction_results.html",
+        screen_name_a=screen_name_a,
+        screen_name_b=screen_name_b,
+        tweet_text=tweet_text,
+        screen_name_most_likely=result[0]
+    )
