3ba534f764baf547d9e9c745ceb36ca04661e528
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-29 23:23:18 -0400

    Customize database

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,5 +1,7 @@
 # web_app/__init__.py
 
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
 from web_app.models import db, migrate
@@ -9,14 +11,16 @@ from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.admin_routes import admin_routes
 from web_app.routes.stats_routes import stats_routes
 
-DATABASE_URI = "sqlite://///Users/mjr/Desktop/twitoff-dspt4/twitoff_development.db"
-SECRET_KEY = "super secret"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = SECRET_KEY # enable flash messaging via sessions
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
